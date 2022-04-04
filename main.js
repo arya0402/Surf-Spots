30d67284126616613c828169d68c1d4297fd8717
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-04-03 20:41:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function c_state() {
+    window.location.replace("california.html");
+}
+
+function f_state() {
+    window.location.replace("florida.html");
+}
+
+function nc_state() {
+    window.location.replace("north carolina.html");
+}
+
+function ny_state() {
+    window.location.replace("new york.html");
+}
+
+function nj_state() {
+    window.location.replace("new jersey.html");
+}
+
+function h_state() {
+    window.location.replace("hawaii.html");
+}
+
+function back() {
+    window.location.replace("index.html");
+}
